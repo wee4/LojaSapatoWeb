969782d384fda1a92729bcc16d6a7756331e61a1
wee4 <40077734+wee4@users.noreply.github.com>
2018-09-04 09:23:08 -0300

    Add files via upload

---
+++ LojaSapatoWeb/LojaSapatoModel/Entity/Client.cs
@@ -0,0 +1,16 @@
+﻿using LojaSapatoModel.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaSapatoModel.Entity
+{
+    class Client : GenericModelImpl
+    {
+        public String nome;
+        public String cpf;
+        public int idade;
+    }
+}
